8bf57b1587599e479465c2fc0f712f3c59591773
William Hancock <williamhancock328@gmail.com>
2023-03-15 19:33:39 -0400

    added empty agent class

---
+++ src/csc3335_project3/Snookie.java
@@ -0,0 +1,22 @@
+
+package csc3335_project3;
+
+import csc3335.gipf_game.GipfGame;
+import csc3335.gipf_game.GipfPlayable;
+
+/**
+ *
+ * @author William Hancock, Antonio Craveiro, Ryan Canfield
+ */
+public class Snookie implements GipfPlayable {
+
+    public Snookie(GipfGame g) {
+        
+    }
+
+    @Override
+    public String makeGipfMove(int curPlayer) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+}
