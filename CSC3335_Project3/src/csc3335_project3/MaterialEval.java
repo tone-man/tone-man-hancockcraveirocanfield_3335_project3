93f86c798e5c00fae804d63c879319e85f3e2265
Toni <craveiroa@merrimack.edu>
2023-03-17 16:31:50 -0400

    Setup MaterialEval
    Now Reflects UML

---
+++ CSC3335_Project3/src/csc3335_project3/MaterialEval.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package csc3335_project3;
+
+/**
+ * A basic implementation of eval that considers keeping material only.
+ * @author Antonio Craveiro
+ */
+public class MaterialEval implements Evaluable{
+
+    @Override
+    public int eval(Game g, State s) {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+}
