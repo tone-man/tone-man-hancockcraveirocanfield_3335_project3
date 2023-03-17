ccd80c030c4951b96f17a52fbe54aeeb82aac6ae
Toni <craveiroa@merrimack.edu>
2023-03-17 16:29:33 -0400

    Setup State + Agent
    Now reflects UML

--- CSC3335_Project3/src/csc3335_project3/Snookie.java
+++ CSC3335_Project3/src/csc3335_project3/Snookie.java
@@ -5,18 +5,40 @@ import csc3335.gipf_game.GipfGame;
 import csc3335.gipf_game.GipfPlayable;
 
 /**
- *
+ * Our Agent to compete in the tournament
  * @author William Hancock, Antonio Craveiro, Ryan Canfield
  */
 public class Snookie implements GipfPlayable {
 
+    private Searchable algorithm;
+    private Game game;
+    private State curState;
+    private boolean debug = false;
+    
     public Snookie(GipfGame g) {
         
     }
-
+    
+    /**
+     * Toggles the debugger
+     */
+    public void toggleDebug() {
+        debug = !debug;
+    }
+    
     @Override
     public String makeGipfMove(int curPlayer) {
         throw new UnsupportedOperationException("Not supported yet."); 
     }
+    
+    /**
+     * Searches the state space and returns the next best move.
+     * @return String - the best action to be taken
+     */
+    private String search(){
+      throw new UnsupportedOperationException("Not supported yet.");  
+    }
+
+
 
 }
--- CSC3335_Project3/src/csc3335_project3/State.java
+++ CSC3335_Project3/src/csc3335_project3/State.java
@@ -5,9 +5,19 @@
 package csc3335_project3;
 
 /**
- *
+ * Our state object, or node, for the game
  * @author ncrav
  */
 public class State {
     
+    private int[] peicesLeft;
+    private int[] gipfRemaining;
+    private int[][] board;
+    private int turn;
+    private String prevAction;
+    
+    @Override
+    public boolean equals(Object otherObject) {
+              throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
