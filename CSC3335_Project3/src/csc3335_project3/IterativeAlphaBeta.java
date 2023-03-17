35a8dc013b73ceaa3c7f5967377be8c229345cbe
toni_craveiro <craveiroa@merrimack.edu>
2023-03-17 11:09:32 -0400

    Created all Files for UML Objects
    I only created all the object we will be using for this project. We do not need kissel's jar. Instead just rip the tuple code from him, and then go from there.

---
+++ CSC3335_Project3/src/csc3335_project3/IterativeAlphaBeta.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package csc3335_project3;
+
+/**
+ *
+ * @author ncrav
+ */
+public class IterativeAlphaBeta {
+    
+}
---
+++ CSC3335_Project3/src/csc3335_project3/SearchAble.java
@@ -0,0 +1,14 @@
+package csc3335_project3;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author ncrav
+ */
+public interface SearchAble {
+    
+}
