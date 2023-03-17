e7553d602a768651561cba664f8b6915ac440e02
Toni <craveiroa@merrimack.edu>
2023-03-17 16:21:08 -0400

    Tuple Constructed + UML
    Finished Tuple Class, reflected in UML

--- CSC3335_Project3/src/csc3335_project3/Tuple.java
+++ CSC3335_Project3/src/csc3335_project3/Tuple.java
@@ -4,10 +4,59 @@
  */
 package csc3335_project3;
 
+import java.util.Objects;
+
 /**
- *
- * @author ncrav
+ * Tuple is a set of two Objects.
+ * @author Antonio Craveiro
+ * @param <T1>
+ * @param <T2>
  */
-public class Tuple {
+public class Tuple<T1, T2> {
+    private T1 first;
+    private T2 second;
     
+    public Tuple(T1 first, T2 second){
+        this.first = first;
+        this.second = second;
+    }
+
+    /**
+     * Gets the first entry.
+     * @return T1
+     */
+    public T1 getFirst() {
+        return first;
+    }
+
+    /**
+     * Sets the first entry.
+     * @param first
+     */
+    public void setFirst(T1 first) {
+        this.first = first;
+    }
+
+    /**
+     * Gets the second entry.
+     * @return T2
+     */
+    public T2 getSecond() {
+        return second;
+    }
+
+    /**
+     * Sets the second entry.
+     * @param second
+     */
+    public void setSecond(T2 second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Tuple{" + "first=" + first + ", second=" + second + '}';
+    }
+    
+
 }
