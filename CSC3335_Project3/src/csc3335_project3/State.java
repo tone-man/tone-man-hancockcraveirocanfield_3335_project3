3e67c483b409e84809ace2c9dda14366f30847c3
Toni <craveiroa@merrimack.edu>
2023-03-18 20:36:37 -0400

    State Implementation
    Decided its easier to just make all state variables public, and then simply built a constructor. May need to tweak prevAction to Tuple so state and action can be retreived.

--- CSC3335_Project3/src/csc3335_project3/State.java
+++ CSC3335_Project3/src/csc3335_project3/State.java
@@ -4,6 +4,8 @@
  */
 package csc3335_project3;
 
+import csc3335.gipf_game.GipfGame;
+
 /**
  * Our state object, or node, for the game
  * @author ncrav
@@ -12,15 +14,42 @@ public class State {
     
     public int[] piecesLeft;
     public int[] gipfsRemaining;
+    public int[] boardMaterial;
     public int[][] board;
-    public int plr;
+    public int turn;
     public String prevAction;
     
-    public State(int[] piecesLeft, int[] gipfsRemaining, int[][] board, 
-            int plr, String prevAction) 
+    public State(int[][] board, int[] piecesLeft, int turn) 
     {
+        this.piecesLeft = piecesLeft;
+        this.boardMaterial = new int[2]; //tracks pieces on board?
+        this.gipfsRemaining = new int[2];
+        this.board = new int[9][];
+        this.turn = turn;
+        this.prevAction = null; //does this work for parenting? Prolly not...
         
-    }
+        //read the board, and build a deep copy while incrementing piece counts
+        for(int row = 0; row < board.length; row++) {
+            for(int col = 0; col < board[row].length; col++){
+                int piece = board[row][col];
+                
+                if (piece == GipfGame.WHITE_GIPF)
+                    this.gipfsRemaining[0]++;
+                else if (piece == GipfGame.BLACK_GIPF)
+                    this.gipfsRemaining[1]++;
+                else if (piece == GipfGame.WHITE)   
+                    this.boardMaterial[0]++;
+                else if (piece == GipfGame.BLACK)
+                    this.boardMaterial[1]++;
+                //otherwise the value is empty
+                
+                //insert value into table
+                this.board[row][col] = piece;
+            }//end of col
+        }//end of row
+        
+    }//end of constructor
+    
     @Override
     public boolean equals(Object otherObject) {
               throw new UnsupportedOperationException("Not supported yet.");
