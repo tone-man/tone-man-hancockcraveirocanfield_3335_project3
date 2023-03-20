3f4a4dc409e6332b5eb7ecd688fb171650cf1131
williamhancock328 <williamhancock328@gmail.com>
2023-03-20 19:27:24 -0400

    added my probably broken mini max algorithm

--- CSC3335_Project3/src/csc3335_project3/IterativeMinMax.java
+++ CSC3335_Project3/src/csc3335_project3/IterativeMinMax.java
@@ -21,7 +21,33 @@ public class IterativeMinMax implements Runnable, Searchable{
 
     @Override
     public String search(Playable game, State state) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return String.valueOf(IterMinMax(game, state, 5, 0));
+    }
+    
+    private int IterMinMax(Playable game, State state, int depth, int player){
+        //minimax implementation
+        //https://www.javatpoint.com/mini-max-algorithm-in-ai
+        if (depth == 0 || game.isTerminal(state)) {
+            return game.utility(state, player);
+        }
+        if (player == 0) {
+            int maxEva = Integer.MIN_VALUE;
+            for (String move : game.getActions(state)) {
+                State child = game.result(state, move);
+                int eva = IterMinMax(game, child, depth - 1, 1);
+                maxEva = Math.max(maxEva, eva);
+            }
+            return maxEva;
+        }
+        else {
+            int minEva = Integer.MAX_VALUE;
+            for (String move : game.getActions(state)) {
+                State child = game.result(state, move);
+                int eva = IterMinMax(game, child, depth - 1, 0);
+                minEva = Math.min(minEva, eva);
+            }
+            return minEva;
+        }
     }
     
     private Tuple<State, Integer> maxValue(Playable game, State state){
