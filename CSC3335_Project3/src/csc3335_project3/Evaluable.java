6673c56ff62c5295d7225832c20812dc9956c328
Canfieldr <canfieldr@merrimack.edu>
2023-03-21 18:59:48 -0400

    Starting working out the Eval and Utility methods
    Wrote pseudo code for the two functions. Starting with something simple to get it running but was also starting thinking about other utility values for other actions

--- CSC3335_Project3/src/csc3335_project3/Evaluable.java
+++ CSC3335_Project3/src/csc3335_project3/Evaluable.java
@@ -16,5 +16,17 @@ public interface Evaluable {
      * @param s
      * @return
      */
+    // eval(Game: game, s : State): int
     public int eval(Playable g, State s);
+    /**
+        //run untility here or previously 
+        State bestState = 0;
+        for(int i = 0; state[i]; i++){//go through all the state's utiliy values
+            
+            if(i > bestState){
+                bestState = i
+            }// evaluating all the states
+     //retruns the best state/move to make
+    */      
+    
 }
